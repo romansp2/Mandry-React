4590ea697874f1cd59e68d549329c1f96eb52598
Pavel Roman <romansp2@gmail.com>
2018-02-02 17:57:34 +0300

    React+Redux added

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,2 +1,11 @@
 class Tour < ApplicationRecord
+  validates :name, presence: true, lenght:{minimum: 10,maximum: 50}
+  validates :description, presence: true, lenght:{minimum: 15,maximum: 200}
+  validates :price, presence: true, numericality: {greater_than_or_equal_to: 1}
+  validates :cover, allow_blank: true, format: {
+      with: %r{\.(gif|jpg|png)$}i,
+      message: 'must be a URL for GIF, DPG or PNG image.'
+  # URL должен указывать на изображение формата GIF, UPG или PNG
+  }
+
 end
